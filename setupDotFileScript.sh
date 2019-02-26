a8207e7a8644be98b3470188638aa73466e20170
simon <simon@pixelpox.co.uk>
2019-02-26 20:42:59 +0000

    add new file links

--- setupDotFileScript.sh
+++ setupDotFileScript.sh
@@ -1,2 +1,12 @@
+#¡/bin/bash
+
+#VI
 ln -s ~/github/dotfiles/.vi/.vimrc ~/.vimrc
-ln -s ~/github/dotfiles/.ssh/config ~/.ssh
+
+#SSH
+mkdir -p ~/.ssh
+ln -s ~/github/dotfiles/.ssh/config ~/.ssh/config
+
+#PROFILE
+ln -s ~/github/dotfiles/.mac/.profile ~/.profile
+ln -s ~/github/dotfiles/.mac/.bash_aliases ~/.bash_aliases
