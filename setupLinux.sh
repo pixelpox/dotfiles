7ad8a1e0ffc4a6a5d094e58de4760352de911520
Simon <simon@pixelpox.co.uk>
2019-04-02 11:35:52 +0100

    added authorized_keys

--- setupLinux.sh
+++ setupLinux.sh
@@ -9,3 +9,5 @@ ln -s ~/github/dotfiles/.vi/.vimrc ~/.vimrc
 #SSH
 mkdir -p ~/.ssh
 ln -s ~/github/dotfiles/.ssh/config ~/.ssh/config
+mv ~/.ssh/authorized_keys ~/.ssh/authorized_keys.bak
+ln -s ~/github/dotfiles/.ssh/simon_authorized_keys ~/.ssh/authorized_keys
